15cf8aebadf667b10f5cf7df9d97552f259e2869
Akber Jagannath <akberjag@gmail.com>
2026-02-28 23:50:51 +0000

    initial commit

---
+++ apps/web/src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import WrappedApp from "@repo/app";
+import "./globals.css";
+
+createApp(WrappedApp).mount("#app");
---
+++ packages/app/src/Root.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { Button } from "@repo/ui";
+import { Badge } from "@repo/ui";
+</script>
+
+<template>
+  <div class="p-6 space-y-4">
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+    <Button>Hello from UI button</Button>
+
+    <Badge variant="default | outline | secondary | destructive"> Badge </Badge>
+
+    <Badge as-child>
+      <a href="#">Badge</a>
+    </Badge>
+  </div>
+</template>
